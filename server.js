aad221b00656732e5f8b73b2fb1ba33194697d6c
EdGrLab <grabchak1999@gmail.com>
2016-07-23 22:54:56 +0300

    changed design of application

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname+'/public'));
-var server = app.listen(3000, function () {
+var server = app.listen(proces.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
